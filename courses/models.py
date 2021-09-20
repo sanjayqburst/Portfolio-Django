9c1d3f715753ff6cb4bc2896615cd813d170725d
sanjayqburst <sanjay.mohan@qburst.com>
2021-09-20 14:23:13 +0530

    Created a model and views for courses

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    image=models.ImageField(upload_to='images/')
+    summary=models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.summary
+
