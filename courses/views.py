44db3ca2befeb11e25315fb4002d71c561478d1d
sanjayqburst <sanjay.mohan@qburst.com>
2021-09-20 14:58:35 +0530

    Added template files and images to the project

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    courses=Course.objects
+
+    return render(request,'courses/index.html',{'courses':courses})
