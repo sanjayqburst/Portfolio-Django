d0625975cddde0df331b87de13c773e016d4b180
sanjayqburst <sanjay.mohan@qburst.com>
2021-09-20 15:46:22 +0530

    Added detail page and updated urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -4,5 +4,6 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('<int:course_id>',views.detail,name='detail'),
 ]
